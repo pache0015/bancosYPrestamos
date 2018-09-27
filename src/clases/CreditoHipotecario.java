a5216fef8b480dd546cd6a2bddda6361d7d186f2
Facundo <Facundo@DESKTOP-8UCO11E>
2018-09-26 21:13:27 -0300

    Herencias de Credito.

---
+++ src/clases/CreditoHipotecario.java
@@ -0,0 +1,8 @@
+package clases;
+
+public class CreditoHipotecario extends Credito{
+
+	public CreditoHipotecario(Float monto, Integer cuotas) {
+		super(monto, cuotas);
+	}
+}
---
+++ src/clases/CreditoPersonal.java
@@ -0,0 +1,9 @@
+package clases;
+
+public class CreditoPersonal extends Credito {
+	
+	public CreditoPersonal(Float monto, Integer cuotas) {
+		super(monto, cuotas);
+	}
+
+}
